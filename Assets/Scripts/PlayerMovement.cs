115a54e4104cd6587ce1f7286119d0188f67be35
Daiwik Bhola <104344893+daiwik-the-creator@users.noreply.github.com>
2024-02-04 02:39:31 -0800

    Movement, Peck, Assets
    Re-did Movement,
    Added Jump,
    Peck,
    Replaced and added art assets.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,87 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector2 movement;
-    Vector2 jump;
-    Rigidbody2D myRb;
-    public float playerSpeed = 2.0f;
-    public float JumpForce = 3.0f;
+
+    private float horizontal;
+    private float playerSpeed = 8f;
+    private float jumpForce = 16f;
+    private bool isFacingRight = true;
+    private bool canJump = false;
+
+    [SerializeField] private Rigidbody2D myRb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    
 
 
     private void Start()
     {
         // get the player's rigidbody
         myRb = GetComponent<Rigidbody2D>();
-        jump.y = JumpForce;
+
     }
-   
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if ((Input.GetKeyDown(KeyCode.UpArrow)) && IsGrounded())
+        {
+            myRb.velocity = new Vector2(myRb.velocity.x, jumpForce);
+        } 
+        else if ((Input.GetKeyDown(KeyCode.UpArrow)) && canJump)
+        {
+            myRb.velocity = new Vector2(myRb.velocity.x, 16f);
+            canJump = false;
+        }
+
+        if (IsGrounded())
+        {
+            canJump = true;
+        }
+
+        Flip();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetScene();
+        }
+
+    }
+
+    private void ResetScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
     void FixedUpdate()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        myRb.MovePosition(myRb.position + movement * playerSpeed * Time.deltaTime);
+        myRb.velocity = new Vector2(horizontal * playerSpeed, myRb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 2.5f, groundLayer);
+
+        return hit.collider != null;
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
-            myRb.AddForce(jump, ForceMode2D.Impulse);
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= - 1f;
+            transform.localScale = localScale;
         }
     }
+
 }
