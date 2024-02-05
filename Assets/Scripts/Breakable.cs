ab808456a13e0ba26776ec8375a201854d326148
Daiwik Bhola <104344893+daiwik-the-creator@users.noreply.github.com>
2024-02-04 17:20:39 -0800

    x

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+
+    bool isShaking = false;
+    float shakeAmount = .1f;
+    float shakeTime = .25f;
+    Vector2 startPos;
+
+    int hit = 0;
+    [SerializeField] private int hitThreshold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isShaking)
+        {
+            transform.position = startPos + UnityEngine.Random.insideUnitCircle * shakeAmount;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "PeckHitbox")
+        {
+            //damage
+            isShaking = true;
+            hit += 1;
+            Invoke("StopShaking", shakeTime);
+
+        }
+    }
+
+    void StopShaking()
+    {
+        isShaking = false;
+        transform.position = startPos;
+        if (hit >= hitThreshold)
+        {
+            Object.Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Peck.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.U2D;
+using UnityEngine;
+using System.IO;
+
+public class Peck : MonoBehaviour
+{
+    private bool isPecking = false;
+
+    [SerializeField] private Rigidbody2D myRb;
+    [SerializeField] private float peckForce;
+    [SerializeField] private GameObject peckHitBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        peckHitBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightShift) && !isPecking)
+        {
+            isPecking = true;
+            StartCoroutine(DoPeck());
+
+        }
+    }
+
+    IEnumerator DoPeck()
+    {
+        peckHitBox.SetActive(true);
+        yield return new WaitForSeconds(.2f);
+        peckHitBox.SetActive(false);
+        ResetPeck();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        if (collision.tag == "Peckable")
+        {
+   
+            Rigidbody2D otherRb = collision.GetComponent<Rigidbody2D>();
+            if (otherRb != null)
+            {
+                
+                Vector2 peckDirection = myRb.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+                otherRb.AddForce(peckDirection * peckForce, ForceMode2D.Impulse);
+            }
+        }
+        
+    }
+
+private void ResetPeck()
+    {
+        isPecking = false;
+    }
+
+}
