605e7ae09824f615a680a5b06ee9bef667811978
Daiwik Bhola <104344893+daiwik-the-creator@users.noreply.github.com>
2024-02-25 20:37:44 -0800

    Live Wire #2

--- Assets/Scripts/LiveWire.cs
+++ Assets/Scripts/LiveWire.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class LiveWire : MonoBehaviour
 {
-    bool isLive = false;
+    Color c;
+    public bool isLive = false;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
+        c = gameObject.GetComponent<SpriteRenderer>().color;
         StartCoroutine(Live());
     }
 
@@ -18,7 +20,9 @@ public class LiveWire : MonoBehaviour
 
         if (isLive)
         {
-            gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+            /*gameObject.GetComponent<SpriteRenderer>().color = Color.blue;*/
+            gameObject.GetComponent<SpriteRenderer>().color = c;
+
         } else
         {
             gameObject.GetComponent<SpriteRenderer>().color = Color.black;
@@ -26,6 +30,24 @@ public class LiveWire : MonoBehaviour
 
         yield return new WaitForSeconds(1.5f);
         StartCoroutine(Live());
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("enteres");
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("Coll");
+        if (isLive)
+        {
+            Debug.Log(other.gameObject.CompareTag("Player"));
+            if (other.gameObject.CompareTag("Player"))
+            {
+                other.GetComponent<PlayerStats>().InflictDamage(1);
+            }
+        }
     }
 
 }
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    [SerializeField] int startHealth;
+    [SerializeField] int startEggCount;
+    public int health = 0;
+    private int eggCount = 0;
+    private List<GameObject> inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = startHealth;
+        eggCount = startEggCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InflictDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Debug.Log("ded");
+            gameObject.transform.Rotate(0, 0, -90);
+        }
+    }
+}
