dff6cfc952bc609de1ad2dc99291260079bdbbaa
HELIX-007 <darkjoker1441@gmail.com>
2024-03-12 15:56:15 -0700

    .

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEditor.Experimental.GraphView;
+
 using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
