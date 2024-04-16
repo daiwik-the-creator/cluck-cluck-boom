49b2c8731aa9b8616e0713bc690e7536572a319f
Daiwik Bhola <104344893+daiwik-the-creator@users.noreply.github.com>
2024-04-15 21:59:49 -0700

    Level End

--- Assets/Scripts/ElevatorLoader.cs
+++ Assets/Scripts/ElevatorLoader.cs
@@ -7,6 +7,8 @@ public class ElevatorLoader : MonoBehaviour
     //private int goldeneggCcount = 0;
     [SerializeField] private int GoldenEggsRequired;
     [SerializeField] private AudioSource levelComplete;
+    [SerializeField] public bool isEnd = false;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "Player")
@@ -24,7 +26,14 @@ public class ElevatorLoader : MonoBehaviour
     {
         levelComplete.Play();
         yield return new WaitForSeconds(t);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (isEnd)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - SceneManager.GetActiveScene().buildIndex);
+        } else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
 
 
 
